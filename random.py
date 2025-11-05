419731e1a4f483ec5530ba2b0e7b2326886b345a
linuxistliebe <linuxistliebe@gmail.com>
2025-11-05 21:38:49 +0530

    first commit

--- random.py
+++ random.py
@@ -1,6 +1,13 @@
 import random
 import time
+n=0
 while True:
-    a=random.randint(1,3)
-    print(a)
+    a,b,c=random.randint(1,3),random.randint(1,3),random.randint(1,3)
+    print(a,b,c)
+    n=n+1
+    if a==1 and b==2 and c==3:
+        print("Match Found")
+        break
     time.sleep(2)
+    
+print("Loop ran",n,"times")
